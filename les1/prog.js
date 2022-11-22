420c19b6ecba58bf35b8fdb8006abf356ebab234
Amber <34961@ma-web.nl>
2022-11-22 11:21:28 +0100

    opdracht 1 gewerkt en af

---
+++ les1/prog.js
@@ -0,0 +1,26 @@
+
+class App
+{
+    runApplication()
+    {
+        console.log("hello word!");
+        console.log("het werkt dus denk ik");
+        let  appNaam = "treinen?";
+        let  versienummer = "0.9";
+        let  versiedatum = "22/11/2022";
+        let  autheur = "Me";
+        let  copyright = "nope:)";
+        let  distributeur = "idk";
+        let  darkmode = true;
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+
+}
+let app = new App();
+app.runApplication();
