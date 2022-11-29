8a17b8c6d8efb025ba5a02e1dd2c95b60ba45804
Amber <34961@ma-web.nl>
2022-11-29 10:59:01 +0100

    bij else if 4? half af

---
+++ ifelse/prog.js
@@ -0,0 +1,32 @@
+
+class App
+{
+    runApplication()
+    {
+        //dit is code commentaar voor javascript
+        let title = document.getElementById("newstitle")
+        let randomGetal = Math.random();
+        if(randomGetal == 0)
+        {
+
+        }
+
+        else if(randomGetal > 0 && randomGetal < 0.2)
+        {
+
+        }
+        
+        else if(randomGetal > 0.2 && randomGetal < 0.6)
+        {
+
+        }
+
+        else if(randomGetal > 0.6)
+        {
+
+        }
+    }
+
+}
+let app = new App();
+app.runApplication();
