e53732fbd0847a1e29ad3fd9157531c6caa215cb
Amber <34961@ma-web.nl>
2022-12-06 10:25:39 +0100

    les3 af en 1 fout er uit

---
+++ functions/prog.js
@@ -0,0 +1,23 @@
+
+class App
+{
+    runApplication()
+    {
+        console.log("hello word!");
+        appNaam()
+        {
+            console.log("MARIO");
+        }
+        hello()
+        {
+            console.log("BWAHAHA!!!")
+        }
+        optellen(x+y)
+        {
+            
+        }
+    }
+
+}
+let app = new App();
+app.runApplication();
--- ifelse/prog.js
+++ ifelse/prog.js
@@ -6,25 +6,28 @@ class App
         //dit is code commentaar voor javascript
         let title = document.getElementById("newstitle")
         let randomGetal = Math.random();
+
         if(randomGetal == 0)
         {
-
+            title.style.backgroundColor = "#7647db";
         }
 
         else if(randomGetal > 0 && randomGetal < 0.2)
         {
-
+            title.style.backgroundColor = "#3869a0";
         }
         
         else if(randomGetal > 0.2 && randomGetal < 0.6)
         {
-
+            title.style.backgroundColor = "#ec0049";
         }
 
-        else if(randomGetal > 0.6)
+        else(randomGetal > 0.6)
         {
-
+            title.style.backgroundColor = "#c982ad";
         }
+        //randomGetal
+        console.log(randomGetal);
     }
 
 }
